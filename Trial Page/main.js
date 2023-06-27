0167ed247b880f8e0725d01ea189fd33d9adba35
Byron-McCann <byron.mccann01@gmail.com>
2023-06-27 15:02:42 +0100

    Finish

--- Trial Page/main.js
+++ Trial Page/main.js
@@ -20,15 +20,25 @@ renderer.setSize(window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
 const geometry = new THREE.CapsuleGeometry(10, 10, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color:"blue"});
-const pill = new THREE.Mesh( geometry, material);
+const material = new THREE.MeshStandardMaterial({ color:"white"});
+const pill1 = new THREE.Mesh( geometry, material);
 
-scene.add(pill)
+const pillTexture = new THREE.TextureLoader().load('pill.png');
+const normalMap = new THREE.TextureLoader().load('norm.png');
 
-const pointlight = new THREE.PointLight("red", 2);
+const spaceTexture = new THREE.TextureLoader().load('spacegoburr.jpg');
+
+const pill = new THREE.Mesh(
+  new THREE.CapsuleGeometry(10, 10, 16, 100),
+  new THREE.MeshBasicMaterial( {map: spaceTexture, normalMap })
+);
+
+scene.add(pill);
+
+const pointlight = new THREE.PointLight("red", 10);
 pointlight.position.set(20,20,20);
 
-const pointlight1 = new THREE.PointLight("green", 4);
+const pointlight1 = new THREE.PointLight("green", 10);
 pointlight1.position.set(-20, 20, 20);
 
 const ambientLight = new THREE.AmbientLight("white", 0.01);
@@ -38,6 +48,21 @@ const lightHelper = new THREE.PointLightHelper(pointlight)
 const lightHelper1 = new THREE.PointLightHelper(pointlight1)
 const gridHelper = new THREE.GridHelper(200 ,50);
 scene.add(lightHelper, lightHelper1);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 25, 24,24, 1000);
+  const material = new THREE.MeshStandardMaterial ( {color: "white"});
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread (200) );
+
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+
 
 const controls = new OrbitControls (camera, renderer.domElement)
 
@@ -50,7 +75,20 @@ function animate() {  /* Animation loop so that the render function does have to
   pill.rotation.y += 0.001;
   pill.rotation.z += 0.01;
 
+  controls.update();
+
   renderer.render( scene, camera);
 }
 
 animate()
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+
+}
+
+document.body.onscroll = moveCamera
